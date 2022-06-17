f0565c370069fa4b01cdfc99d304818586bb9b19
Vahin <danil.e.vakhrushev@gmail.com>
2022-06-18 03:45:16 +0500

    Первая секция завершена

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ new HideOnScroll({
     elementSelector: ".header",
     menuSelector: ".nav",
 });
+
+// import Swiper from "https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js";
+
+const welcomeSlider = new Swiper(".welcome__swiper", {
+    spaceBetween: 20,
+    slidesPerView: "auto",
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-custom-buttom-prev",
+        prevEl: ".swiper-custom-buttom-next",
+    },
+});
